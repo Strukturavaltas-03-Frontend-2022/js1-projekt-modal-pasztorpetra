ac74a70ebb32a18bcb83d22825baadcc92069156
Petra <pasztor.petra1998@gmail.com>
2022-08-12 16:09:30 +0200

    :(

--- main.js
+++ main.js
@@ -4,10 +4,27 @@ const btn = document.querySelector(".katt");
 const closeBtn = document.querySelector(".close");
 
 btn.addEventListener("click", () => {
-    myModal.style.visibility="visible";
+    myModal.classList.add("fadeIn");
+    setTimeout(() => {
+        myModal.style.visibility="visible";
+    }, 500);
+    document.getElementById("myModal").focus();
 });
 
 closeBtn.addEventListener("click", () => {
-    myModal.style.visibility ="hidden";
-});
+    myModal.classList.add("fadeOut");
+    setTimeout (() => {
+        myModal.style.visibility = "hidden"; 
+    }, 500);
+})
+    
+window.onclick = (event) => { 
+    if(event.target == myModal) {
+        myModal.style.display = "none"; } 
+    }
+
+
+
+
+
 
